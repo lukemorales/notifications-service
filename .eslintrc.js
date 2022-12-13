bb6ab0977eb936a92a3fdcfe1d0fda754500b336
Luke Morales <lukemorales@live.com>
2022-12-12 22:12:47 -0300

    Project scaffolding

---
+++ .eslintrc.js
@@ -0,0 +1,390 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['import-helpers', 'no-secrets', '@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+      },
+      { usePrettierrc: false },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'test/**', // tape, common npm pattern
+          'tests/**', // also common npm pattern
+          '**/__tests__/**', // jest pattern
+          '**/__mocks__/**', // jest pattern
+          'test.{js,jsx,ts,tsx}', // repos with a single test file
+          'test-*.{js,jsx,ts,tsx}', // repos with multiple top-level test files
+          '**/*{.,_}{test,spec}.{js,jsx,ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
+          '**/{jest,vitest}.config.{js,ts}', // jest/vitest config
+          '**/{jest,vitest}.setup.{js,ts}', // jest/vitest setup
+          '**/webpack.config.js', // webpack config
+          '**/webpack.config.*.js', // webpack config
+          '**/{rollup,vite}.config.{js,ts}', // rollup/vite config
+          '**/{rollup,vite}.config.*.{js,ts}', // rollup/vite config
+          '**/.eslintrc.js', // eslint config
+        ],
+        optionalDependencies: false,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', mjs: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          [
+            '/^(react|express|fastify)$/',
+            '/^react-(native|dom)$/',
+            '/^(next|@nestjs|@fastify)/',
+          ],
+          'module',
+          [
+            '/^@(api|app|assets|common|components|constants|contexts|features|hooks|mocks|pages|routes|services|styles|types|utils|shared|store)/',
+          ],
+          ['parent', 'sibling'],
+          'index',
+        ],
+        alphabetize: { order: 'ignore', ignoreCase: true },
+      },
+    ],
+    'import/no-anonymous-default-export': [
+      'error',
+      {
+        allowArray: true,
+        allowArrowFunction: false,
+        allowAnonymousClass: false,
+        allowAnonymousFunction: false,
+        allowCallExpression: true,
+        allowLiteral: true,
+        allowObject: true,
+      },
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsForRegex: [
+          '^draft',
+          '(result|map|set|obj|record|sum|group)',
+          '^acc',
+          '.*(Map|Set)$',
+        ],
+      },
+    ],
+    'no-secrets/no-secrets': 'error',
+    'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unneeded-ternary': 'error',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-useless-concat': 'error',
+    'no-useless-constructor': 'off',
+    'prefer-template': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'no-undef': 'off',
+    '@typescript-eslint/array-type': [
+      'warn',
+      { default: 'array-simple', readonly: 'array-simple' },
+    ],
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: { '{}': false },
+      },
+    ],
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/consistent-generic-constructors': 'warn',
+    '@typescript-eslint/consistent-indexed-object-style': 'warn',
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    'default-param-last': 'off',
+    '@typescript-eslint/default-param-last': 'warn',
+    'dot-notation': 'off',
+    '@typescript-eslint/dot-notation': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': 'warn',
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        classes: [
+          // Index signature
+          'signature',
+
+          // Fields
+          'public-static-field',
+          'protected-static-field',
+          'private-static-field',
+
+          'public-decorated-field',
+          'protected-decorated-field',
+          'private-decorated-field',
+
+          'public-instance-field',
+          'protected-instance-field',
+          'private-instance-field',
+
+          'public-abstract-field',
+          'protected-abstract-field',
+
+          'public-field',
+          'protected-field',
+          'private-field',
+
+          'static-field',
+          'instance-field',
+          'abstract-field',
+
+          'decorated-field',
+
+          'field',
+
+          // Static initialization
+          'static-initialization',
+
+          // Constructors
+          'public-constructor',
+          'protected-constructor',
+          'private-constructor',
+
+          'constructor',
+
+          // Getters
+          'public-static-get',
+          'protected-static-get',
+          'private-static-get',
+
+          'public-decorated-get',
+          'protected-decorated-get',
+          'private-decorated-get',
+
+          'public-instance-get',
+          'protected-instance-get',
+          'private-instance-get',
+
+          'public-abstract-get',
+          'protected-abstract-get',
+
+          'public-get',
+          'protected-get',
+          'private-get',
+
+          'static-get',
+          'instance-get',
+          'abstract-get',
+
+          'decorated-get',
+
+          'get',
+
+          // Setters
+          'public-static-set',
+          'protected-static-set',
+          'private-static-set',
+
+          'public-decorated-set',
+          'protected-decorated-set',
+          'private-decorated-set',
+
+          'public-instance-set',
+          'protected-instance-set',
+          'private-instance-set',
+
+          'public-abstract-set',
+          'protected-abstract-set',
+
+          'public-set',
+          'protected-set',
+          'private-set',
+
+          'static-set',
+          'instance-set',
+          'abstract-set',
+
+          'decorated-set',
+
+          'set',
+
+          // Methods
+          'public-static-method',
+          'protected-static-method',
+          'private-static-method',
+
+          'public-decorated-method',
+          'protected-decorated-method',
+          'private-decorated-method',
+
+          'public-instance-method',
+          'protected-instance-method',
+          'private-instance-method',
+
+          'public-abstract-method',
+          'protected-abstract-method',
+
+          'public-method',
+          'protected-method',
+          'private-method',
+
+          'static-method',
+          'instance-method',
+          'abstract-method',
+
+          'decorated-method',
+
+          'method',
+        ],
+        interfaces: {
+          memberTypes: ['signature'],
+          order: 'alphabetically-case-insensitive',
+        },
+        typeLiterals: {
+          memberTypes: ['signature'],
+          order: 'alphabetically-case-insensitive',
+        },
+      },
+    ],
+
+    '@typescript-eslint/method-signature-style': 'warn',
+    '@typescript-eslint/no-confusing-non-null-assertion': 'warn',
+
+    '@typescript-eslint/no-confusing-void-expression': [
+      'warn',
+      { ignoreVoidOperator: true },
+    ],
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'warn',
+    '@typescript-eslint/no-duplicate-enum-values': 'warn',
+    '@typescript-eslint/no-dynamic-delete': 'warn',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      { allowSingleExtends: true },
+    ],
+
+    '@typescript-eslint/no-explicit-any': ['warn', { ignoreRestArgs: true }],
+    'no-extra-parens': 'off',
+    '@typescript-eslint/no-extra-parens': 'warn',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      { ignoreParameters: true },
+    ],
+    '@typescript-eslint/no-invalid-void-type': 'warn',
+    '@typescript-eslint/no-meaningless-void-operator': 'warn',
+    '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error', { ignoreTypeValueShadow: true }],
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'warn',
+    '@typescript-eslint/no-unnecessary-condition': 'warn',
+    '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
+    '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_$|[iI]gnored',
+      },
+    ],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/prefer-function-type': 'warn',
+    '@typescript-eslint/prefer-includes': 'warn',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
+    '@typescript-eslint/prefer-return-this-type': 'warn',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'warn',
+    '@typescript-eslint/prefer-ts-expect-error': 'warn',
+    '@typescript-eslint/require-array-sort-compare': 'warn',
+    '@typescript-eslint/restrict-plus-operands': [
+      'error',
+      { checkCompoundAssignments: true },
+    ],
+    '@typescript-eslint/switch-exhaustiveness-check': 'warn',
+    '@typescript-eslint/unified-signatures': 'warn',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(e2e-spec|spec|test).ts?(x)'],
+      env: {
+        es2022: true,
+        jest: true,
+        node: true,
+      },
+      settings: {
+        jest: {
+          version: 'detect',
+        },
+      },
+      plugins: ['jest', 'jest-formatting'],
+      extends: [
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+        'plugin:jest-formatting/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        'arrow-body-style': 'off',
+        'jest/expect-expect': 'off'
+      },
+    },
+  ],
+};
