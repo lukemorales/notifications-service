bcb87b3daa4d0d609cc4eefbd846cb0805c21a58
Luke Morales <lukemorales@live.com>
2022-12-13 10:38:00 -0300

    Setup nestjs-prisma

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 
+import { PrismaService } from 'nestjs-prisma';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
